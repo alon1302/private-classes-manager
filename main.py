f37a66c9cf7f117f4150e60fecff0372cc209637
alon1302 <66137565+alon1302@users.noreply.github.com>
2023-09-09 15:24:58 +0300

    add some routes to the api, it will change

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from model.models import Lesson, Student
 
 app = FastAPI()
 
+lessons = []
 
-@app.get("/")
+@app.get("/lessons/getAll")
 async def root():
-    return {"message": "Hello World"}
+    return {"lessons": lessons}
+
+@app.get("/lessons/{student_name}")
+async def root(student_name):
+    return {"lessons":  [c for c in lessons if c.student_name == student_name] }
+
+
+@app.post("/lessons/add")
+async def add_class(class_item: Lesson):
+    lessons.append(class_item)
+    return {"message": "class added"}
